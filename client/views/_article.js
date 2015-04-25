1e11314fd24273dcad229f555498f11aa02e9355
Mark Bellaire <mark.bellaire@gmail.com>
2015-04-25 10:21:26 -0400

    Show created time in articles grid

--- client/views/_article.js
+++ client/views/_article.js
@@ -1,6 +1,13 @@
 Template.article.helpers({
   body: function () {
-    console.log($(this.body.value));
+    // Return teaser to article.
     return $(this.body.value).text().substr(0, 200) + '...';
+  },
+  dateCreated: function() {
+    // Create the time output based on the timestamp from article.
+    var date = moment.unix(this.created).format("MMMM DD, YYYY");
+
+    // Return date with clock image.
+    return '<span class="glyphicon glyphicon-time"></span> ' + date;
   }
 });
