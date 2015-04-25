893c927c0b5c622f448bfca4fbcc3b71c17e27de
Mark Bellaire <mark.bellaire@gmail.com>
2015-04-25 10:35:59 -0400

    Navbar, search, and iron:router
    The navbar, right now, has an img in it with a src to /icon.svg. There
    will need to be a setting for this image so that one can change the
    image name and path to their liking.
    
    The search is buggy. The Isotope animation is the only oddity.
    Considering using Freewall to create the grid layout instead of Isotope.

---
+++ client/views/_navbar.js
@@ -0,0 +1,9 @@
+Template.navbar.onCreated(function() {
+  Session.setDefault('searchString', '');
+});
+
+Template.navbar.events({
+  'keyup #search-bar':  function(event) {
+    Session.set('searchString', $('#search-bar').val());
+  },
+});
--- client/views/articles.js
+++ client/views/articles.js
@@ -4,18 +4,33 @@ Template.articles.onCreated(function () {
 
 Template.articles.helpers({
   articles: function() {
-    console.log('articles returned'); // DELETE
-    return Articles.find();
+    var searchQuery = '.*' + Session.get('searchString') + '.*';
+    return Articles.find({title: {$regex: searchQuery, $options: "i"}}, {sort: {created: -1}});
   }
 });
 
 Template.articles.onRendered(function() {
   var self = this;
   articleWrapper = self.$('#articles-list');
-  article = articleWrapper.find('.article');
+  articles = articleWrapper.find('.article');
   $container = $('#articles-list').isotope({
     itemSelector: '.article',
     layoutMode: 'masonry',
+    transitionDuration: '0.5s',
   });
-  console.log('file rendered'); // DELETE
+  articles.velocity('transition.flipXIn', {
+    stagger: 25,
+    duration: 250,
+    drag: true,
+  });
+  this.find('#articles-list .row')._uihooks = {
+    insertElement: function(article, next) {
+      $(article).insertBefore(next);
+      $container.isotope('insert', $(article)).isotope('layout');
+    },
+    removeElement: function(article) {
+      $(article).remove();
+      $container.isotope('layout');
+    }
+  };
 });
