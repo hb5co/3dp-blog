8d09b15b65faf23564b387a0e44b10cc1f414306
Mark Bellaire <mark.bellaire@gmail.com>
2015-06-04 11:54:47 -0400

    Create center title helper
    The helper for using "center" setting in settings.json needed to be created.

--- client/views/_fullArticle.js
+++ client/views/_fullArticle.js
@@ -1,3 +1,9 @@
 Template.fullArticle.onCreated(function() {
   this.subscribe('articles');
 });
+
+Template.fullArticle.helpers({
+  centerTitle: function() {
+    return Meteor.settings.public.centerTitle;
+  }
+});
